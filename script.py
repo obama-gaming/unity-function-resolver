1889ec102a44ed7e0ed35b871efbdadea75774b2
daum <daumm@protonmail.com>
2020-04-04 02:01:39 -0500

    imports

--- script.py
+++ script.py
@@ -1,18 +1,33 @@
 import idc
 import idaapi
 
-def rename(address, alias):
-    MakeCode(address)
-    MakeFunction(address)
+def set_name(address, alias):
     MakeNameEx(address, alias, SN_NOWARN)
-    return
 
+def find_function(sig, name, operand):
+    address = GetOperandValue(FindBinary(0, SEARCH_DOWN, sig), operand)
+    set_name(address, name)
+    return address
 
-export_function = GetOperandValue(FindBinary(0, SEARCH_DOWN, "E9 ? ? ? ? CC F2 0F 10 05 ? ? ? ?"), 0)
-rename(export_function, "export_function");
-    
-export_xrefs = XrefsTo(export_function)
-for xref in export_xrefs:
-    name = GetManyBytes(GetOperandValue(xref.frm - 7, 1), 64)
-    if (name.startswith("UnityEngine")):
-        rename(GetOperandValue(xref.frm - 14, 1), name)
+def resolve(function, prefix, callback):
+    xrefs = XrefsTo(function)
+    for xref in xrefs:
+        counter = 0;
+        while (counter < 20):
+            name = GetManyBytes(GetOperandValue(xref.frm - counter, 1), 64)
+            if (name.startswith(prefix)):
+                callback(xref.frm, name)
+                break           
+            counter += 1
+
+def import_callback(address, name):
+    set_name(GetOperandValue(address + 5, 0), name)
+
+def export_callback(address, name):
+    set_name(GetOperandValue(address - 14, 1), name)
+
+exports = find_function("E9 ? ? ? ? CC F2 0F 10 05 ? ? ? ?", "find_export", 0)
+resolve(exports, "UnityEngine", export_callback)
+
+imports = find_function("E8 ? ? ? ? 48 8B 0E 48 89 01", "find_import", 0)
+resolve(imports, "il2cpp", import_callback)
